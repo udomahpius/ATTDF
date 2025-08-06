3149e0fab81ae53852c9c847cf780d484ff3053c
Pius Udomah <udomahpius5@gmail.com>
2025-08-06 10:47:59 +0100

    init

--- js/main.js
+++ js/main.js
@@ -133,6 +133,17 @@
     });
 
 
+      /**
+   * Frequently Asked Questions Toggle
+   */
+  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+    faqItem.addEventListener('click', () => {
+      faqItem.parentNode.classList.toggle('faq-active');
+    });
+  });
+
+
+
 
 })(jQuery);
 
